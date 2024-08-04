c9521c1be34a858e0e4461f230ad0641797770fb
khaled latrach <khaledlatrach9999@gmail.com>
2024-08-04 22:59:26 +0100

    Add navbar
    Add navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,1 +1,19 @@
-<div>the navbar</div>
+<div class="font-inter">
+	<div class="flex justify-between items-center h-24 m-auto px-6">
+		<!-- log -->
+		<div class="flex flex-col text-teal-900 font-bold text-2xl">
+			<div class="text-teal-500">Doc</div>
+			<div class="-mt-2 text-teal-900">Finder</div>
+		</div>
+
+		<!-- menu -->
+		<!-- <div>Menu</div> -->
+
+		<!-- sign -->
+		<div class=" text-teal-900 font-bold">
+			<a href="">Sign in</a>
+			|
+			<a href="">Log in</a>
+		</div>
+	</div>
+</div>
