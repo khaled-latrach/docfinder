f01de3c877f06582bec50880e4ef0efe19b26f4c
khaled latrach <khaledlatrach9999@gmail.com>
2024-08-04 23:08:09 +0100

    Add hero section
    Add hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Hero from '$lib/components/hero.svelte';
 </script>
 
+<Navbar />
+<Hero />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,15 @@
 	export let data;
 </script>
 
-<ul>
-	{#each data.docs as docs}
-		<li>{docs.docFName}</li>
-	{/each}
-</ul>
+<div class="bg-teal-900 h-screen">
+	<p class="text-white text-2xl">
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat et deserunt dolorum odit
+		facilis temporibus, voluptate hic alias, fugit maxime iure eum ex dolorem facere magnam id
+		expedita nobis
+	</p>
+	<!-- <ul> -->
+	<!-- 	{#each data.docs as docs} -->
+	<!-- 		<li>{docs.docFName}</li> -->
+	<!-- 	{/each} -->
+	<!-- </ul> -->
+</div>
